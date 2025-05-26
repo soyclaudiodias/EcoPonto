c2035865c5e5fb30b0e8b86ec98ee7110f58638a
Guillermo02 <62570122+Guillermo02@users.noreply.github.com>
2025-05-25 22:29:22 -0300

    Update database.py

--- database.py
+++ database.py
@@ -1,13 +1,12 @@
 # database.py
-import os
 import mysql.connector
 
 def conectar():
     conexao = mysql.connector.connect(
-        host=os.getenv("MYSQLHOST", "mysql-swww.railway.internal"),
-        user=os.getenv("MYSQLUSER", "root"),
-        password=os.getenv("MYSQLPASSWORD", "1234"),
-        database=os.getenv("MYSQLDATABASE", "railway"),
-        port=int(os.getenv("MYSQLPORT", 3306))
+        host="mysql-swww.railway.internal",
+        user="root",
+        password="1234",
+        database="railway",
+        port=3306
     )
     return conexao
