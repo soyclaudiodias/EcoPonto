aa303d9d27c07b1ec43d154f7a984139faa784b5
Guillermo02 <62570122+Guillermo02@users.noreply.github.com>
2025-05-25 23:07:20 -0300

    Update database.py

--- database.py
+++ database.py
@@ -1,16 +1,21 @@
-import os
-from dotenv import load_dotenv
+# import os
+# from dotenv import load_dotenv
 import mysql.connector
 
-dotenv_path = os.path.join(os.path.dirname(__file__), '..', '.gitignore', '.env')
-load_dotenv(dotenv_path)
+# dotenv_path = os.path.join(os.path.dirname(__file__), '..', '.gitignore', '.env')
+# load_dotenv(dotenv_path)
+MYSQL_DATABASE = "railway"
+MYSQL_HOST = "shortline.proxy.rlwy.net"
+MYSQL_PORT = 38164
+MYSQL_USER = "root"
+MYSQL_PASSWORD = "1234"
 
 def conectar():
     conexao = mysql.connector.connect(
-        host=os.getenv("MYSQL_HOST"),
-        user=os.getenv("MYSQL_USER"),
-        password=os.getenv("MYSQL_PASSWORD"),
-        database=os.getenv("MYSQL_DATABASE"),
-        port=int(os.getenv("MYSQL_PORT"))
+        host="MYSQL_HOST",
+        user="MYSQL_USER",
+        password="MYSQL_PASSWORD",
+        database="MYSQL_DATABASE",
+        port="MYSQL_PORT"
     )
     return conexao
