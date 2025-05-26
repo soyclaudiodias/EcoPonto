0594a87c003671fb2b936470e366b3376a6e836f
Guillermo02 <62570122+Guillermo02@users.noreply.github.com>
2025-05-25 22:15:58 -0300

    Update database.py

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ import mysql.connector
 
 def conectar():
     conexao = mysql.connector.connect(
-        host=os.getenv("MYSQLHOST", "mysql.railway.internal"),
+        host=os.getenv("MYSQLHOST", "shortline.proxy.rlwy.net"),
         user=os.getenv("MYSQLUSER", "root"),
         password=os.getenv("MYSQLPASSWORD", "1234"),
         database=os.getenv("MYSQLDATABASE", "railway"),
